ea88619246f52064adcb6535e10b577607fa8e64
rubbashka <rubbashka@gmail.com>
2015-11-08 21:49:02 +0400

    Added both tasks

---
+++ controller/model/src/main/java/Appointment.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+/**
+ * Created by Ivan.Chikhanov on 08.11.2015.
+ */
+public class Appointment extends Task {
+    private Date duration;
+
+    public Date getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Date duration) {
+        this.duration = duration;
+    }
+}
---
+++ controller/model/src/main/java/Task.java
@@ -0,0 +1,44 @@
+import java.util.Date;
+
+/**
+ * Created by Ivan.Chikhanov on 08.11.2015.
+ */
+public class Task {
+    private int id;
+    private String title;
+    private Date time;
+    private String comment;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
