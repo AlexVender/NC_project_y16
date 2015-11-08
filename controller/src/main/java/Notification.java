498c03eaa19f8a9dc01e8a2b29cb1adda858d7c3
rubbashka <rubbashka@gmail.com>
2015-11-08 22:20:14 +0400

    Added Notification

---
+++ controller/src/main/java/Notification.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Ivan.Chikhanov on 08.11.2015.
+ */
+public class Notification {
+    private Task currentTask;
+
+    public void notificate(){
+
+    }
+
+    public void postpone(){
+
+    }
+
+    public void dismiss(){
+
+    }
+
+    public void getLastTask(){
+
+    }
+
+    public Task getCurrentTask() {
+        return currentTask;
+    }
+
+    public void setCurrentTask(Task currentTask) {
+        this.currentTask = currentTask;
+    }
+}
