d7a08ab81ec6bb3f207139d960a3783c38523eaf
AlexVender <uskov_v50@mail.ru>
2015-11-17 23:31:39 +0400

    Added TaskManagerView and CalendarPanel

---
+++ view/src/main/java/org/netcracker/unc/group16/CalendarPanel.java
@@ -0,0 +1,145 @@
+package org.netcracker.unc.group16;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+
+public class CalendarPanel extends JPanel {
+    private Font font;
+    private GregorianCalendar calendar;
+    private GregorianCalendar presentDay;
+    private int[][] dates;
+
+    private int year;
+    private int month;
+    private int daysInMonth;
+    private int firstWeekday; // 1-7
+
+    private static final String[] weekdays = {
+            "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"
+    };
+
+    private static final int weekdaysHeight = 24;
+
+    private static final Color WEEKENDS_COLOR = new Color(213, 230, 244);
+    private static final Color ANOTHER_MONTH_COLOR = new Color(237,237,237);
+    private static final Color ANOTHER_MONTH_WEEKENDS_COLOR = new Color(225, 233, 240);
+
+    public CalendarPanel() {
+
+//        DeBug
+        year = 2016;
+        month = Calendar.OCTOBER;
+//        #####
+
+        presentDay = new GregorianCalendar();
+        calendar = new GregorianCalendar();
+
+        calendar.setMinimalDaysInFirstWeek(7);
+        setBorder(new LineBorder(Color.black));
+
+        setFont(new Font("Verdana", Font.BOLD, 13));
+
+        calcDaysPositions();
+    }
+
+    private void calcDaysPositions() {
+        calendar.set(presentDay.get(Calendar.YEAR), presentDay.get(Calendar.MONTH), 1);
+
+        daysInMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+//        firstWeekday = calendar.get(Calendar.DAY_OF_WEEK);
+
+        calendar.set(Calendar.WEEK_OF_MONTH, calendar.getActualMinimum(Calendar.WEEK_OF_MONTH));
+        calendar.set(Calendar.DAY_OF_WEEK, calendar.getFirstDayOfWeek());
+
+        dates = new int[6][7];
+        for (int r = 0; r < 6; r++) {
+            for (int c = 0; c < 7; c++) {
+                dates[r][c] = calendar.get(Calendar.DAY_OF_MONTH);
+                calendar.add(Calendar.DAY_OF_MONTH, 1);
+            }
+        }
+        System.out.println();
+    }
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D)g;
+        int height = getHeight();
+        int width = getWidth();
+
+        double xGridStep = width / 7;
+        double yGridStep = (height - weekdaysHeight) / 6;
+
+
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, width, height);
+        g2d.setColor(Color.BLACK);
+
+        // Сглаживание
+        g2d.setRenderingHint ( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
+        // Выключить сглаживание для текста ?
+//        g2d.setRenderingHint ( RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_OFF );
+
+
+        g2d.setColor(WEEKENDS_COLOR);
+        g2d.fillRect((int)(xGridStep*5), weekdaysHeight, (int)(width - xGridStep*5), height - weekdaysHeight);
+        g2d.setColor(Color.BLACK);
+
+
+        boolean isCurMonth = false;
+        for (int r = 0; r < 6; r++) {
+            int yDatePos = (int) (weekdaysHeight + yGridStep*r + (weekdaysHeight + g2d.getFontMetrics().getAscent())/2);
+            for (int c = 0; c < 7; c++) {
+                if (dates[r][c] == 1) {
+                    isCurMonth = !isCurMonth;
+                }
+
+                if (!isCurMonth) {
+                    if (c < 5) {
+                        g2d.setColor(ANOTHER_MONTH_COLOR);
+                    } else {
+                        g2d.setColor(ANOTHER_MONTH_WEEKENDS_COLOR);
+                    }
+
+
+                    g2d.fillRect(
+                            (int) (xGridStep * c ),
+                            (int) (weekdaysHeight + yGridStep * r),
+                            (int) xGridStep,
+                            (int) yGridStep
+                    );
+                }
+
+                int xDatePos = (int) (xGridStep*(c+1) - g2d.getFontMetrics().stringWidth(String.valueOf(dates[r][c])) - 5);
+                g2d.setColor(Color.BLACK);
+                g2d.drawString(String.valueOf(dates[r][c]), xDatePos, yDatePos);
+            }
+        }
+
+        // Сетка
+        //   Вертикальные линии
+        for (int i = 1; i <= 6; i++) {
+            g2d.drawLine((int) (xGridStep * i), 0, (int) (xGridStep * i), height);
+        }
+        //   Горизонтальные линии
+        for (int i = 0; i < 6; i++) {
+            g2d.setColor(new Color(166, 166, 166));
+            g2d.drawLine(0, weekdaysHeight*2 + (int)(yGridStep*i), width, weekdaysHeight*2 + (int)(yGridStep*i));
+            g2d.setColor(Color.BLACK);
+            g2d.drawLine(0, weekdaysHeight + (int)(yGridStep*i), width, weekdaysHeight + (int)(yGridStep*i));
+        }
+
+        // Подписи дней недель
+        for (int i = 0; i < 7; i++) {
+            int x = (int)(xGridStep*i + (xGridStep - g2d.getFontMetrics().stringWidth(weekdays[i])) / 2) + 1;
+            int y = (weekdaysHeight + g2d.getFontMetrics().getAscent())/2;
+            g2d.drawString(weekdays[i], x, y);
+        }
+
+//        g2d.drawString("W: " + width + "  H: " + height, 5, 70);
+    }
+
+}
--- view/src/main/java/org/netcracker/unc/group16/TaskManagerView.java
+++ view/src/main/java/org/netcracker/unc/group16/TaskManagerView.java
@@ -1,12 +1,107 @@
-/**
- * Created by Ivan.Chikhanov on 09.11.2015.
- */
-
 package org.netcracker.unc.group16;
 
-public class TaskManagerView implements ProgramInterface {
-    public void drawNotificate(){
+import sun.java2d.HeadlessGraphicsEnvironment;
+import sun.java2d.SunGraphicsEnvironment;
 
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+
+public class TaskManagerView implements ProgramInterface {
+    private JFrame mainFrame;
+    private Panel leftControlPanel;
+    private Panel rightControlPanel;
+    private Panel monthYearPanel;
+    private JLabel lblMonthYear;
+
+    public TaskManagerView(TaskManager taskManager) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        initGUI();
+    }
+
+    private void initGUI() {
+        mainFrame = new JFrame("Task manager");
+        mainFrame.setResizable(true);
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//        mainFrame.setPreferredSize(new Dimension(1440, 900));
+        mainFrame.setMinimumSize(new Dimension(750, 650));
+
+        mainFrame.setLayout(new BorderLayout());
+        JPanel contentPane = new JPanel();
+        mainFrame.getContentPane().add(contentPane, BorderLayout.PAGE_START);
+
+        contentPane.setLayout(new GridBagLayout());
+        GridBagConstraints c1 = new GridBagConstraints();
+
+        c1.anchor = GridBagConstraints.NORTH;
+
+// First line
+        leftControlPanel = new Panel();
+        rightControlPanel = new Panel();
+
+        c1.gridwidth = 1;
+        c1.weightx = 1;
+        c1.insets = new Insets(10,10,10,10);
+        c1.anchor = GridBagConstraints.WEST;
+        contentPane.add(leftControlPanel, c1);
+        leftControlPanel.setLayout(new GridBagLayout());
+
+        GridBagConstraints c2 = new GridBagConstraints();
+        c2.gridy = 0;
+        c2.anchor = GridBagConstraints.WEST;
+        c2.gridx = 0;
+        JButton btnCreateTask = new JButton("Создать задачу");
+        leftControlPanel.add(btnCreateTask, c2);
+        c2.gridx = 1;
+        JButton btnViewTask = new JButton("Просмотр задач");
+        leftControlPanel.add(btnViewTask, c2);
+
+        c1.gridx = 1;
+        c1.anchor = GridBagConstraints.EAST;
+        contentPane.add(rightControlPanel, c1);
+        rightControlPanel.setLayout(new GridBagLayout());
+        c2.gridx = 2;
+        JButton btnPrevMonth = new JButton("  <  ");
+        rightControlPanel.add(btnPrevMonth, c2);
+        c2.gridx = 3;
+        JButton btnNextMonth = new JButton("  >  ");
+        rightControlPanel.add(btnNextMonth, c2);
+
+// Second line
+        monthYearPanel = new Panel();
+        c1.gridx = 0;
+        c1.gridy = 1;
+        c1.gridwidth = 2;
+        c1.anchor = GridBagConstraints.CENTER;
+        contentPane.add(monthYearPanel, c1);
+        lblMonthYear = new JLabel("Ноябрь 2015");
+        lblMonthYear.setFont(new Font("Verdana", 0, 15));
+        monthYearPanel.add(lblMonthYear);
+
+
+// Third line
+        CalendarPanel calendarPanel = new  CalendarPanel();
+        mainFrame.add(calendarPanel);
+
+
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+    }
+
+    private static void addAButton(String text, Container container) {
+        JButton button = new JButton(text);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        container.add(button);
+    }
+
+    public void drawNotificate(){
     }
 
     public void drawTaskManager(){
